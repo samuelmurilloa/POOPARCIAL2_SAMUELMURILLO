2f345372db3f233e025f1ac930ad3aedcddb451c
samuelmurilloa <samuel.murillo_a@uao.edu.co>
2026-04-13 10:34:37 -0500

    primer commit, inicializar repo

---
+++ src/main/java/com/mycompany/pooparcial2_samuelmurillo/POOPARCIAL2_SAMUELMURILLO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooparcial2_samuelmurillo;
+
+/**
+ *
+ * @author samue
+ */
+public class POOPARCIAL2_SAMUELMURILLO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
